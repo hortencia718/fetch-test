84938fb216640fec6ed455cfe4b206e742fda3ce
hortencia718 <hortenciaecisneros@gmail.com>
2021-04-07 12:48:51 -0400

    arrow

--- index.js
+++ index.js
@@ -1,42 +1,49 @@
 
 let dogButton = document.querySelector("button#hello-button")
+let dogUl =document.querySelector("#dogs")
+// (1)select/get the element 
 // console.log(dogButton);
 
 
 
 
 
-dogButton.addEventListener("click", function(evt){
+dogButton.addEventListener("click", (evt) =>{
 
    fetch("https://dog.ceo/api/breeds/image/random")
    .then((resp) =>  resp.json())
    .then((dogObj) => {
-       console.log(dogObj)
+       let dogLi = document.createElement("li")
+    //   (2) create the element 
+       let dogImg = document.createElement("img")
+         dogImg.src = dogObj.message
+
+       dogLi.append(dogImg)
+    //    (3)show the element
+       dogUl.append(dogLi)
+    //    console.log(dogLi)
+    //    console.log(dogObj)
    })
-    //   return resp.json() 
-    // console.log("hello from line 14")
-  
-//    let thirdPromiseObj = theSecondPromise.then(function(dogObj){
-//     console.log(dogObj)
+   
   })
- 
+  console.log(dogObj)
   
-})
+// })
 
 
 // just another way to write a fucntion definition 
-function thisIsRegularFunction(arg1){
+// function thisIsRegularFunction(arg1){
 
-}
+// }
 
 // this is a arrow function if therre is no block
 // if you take out the cruly's add the value you want to r/t will r/t that value
-let thisIsRegularFunction = (arg1) => "chicken"
+// let thisIsRegularFunction = (arg1) => "chicken"
 // returns chicken w/o curly's
 // with curly
-let thisIsRegularFunction = (arg1) => {
+// let thisIsRegularFunction = (arg1) => {
 
-}
+// }
 
 // arr.forEach((element)=>{
 // do something with element
